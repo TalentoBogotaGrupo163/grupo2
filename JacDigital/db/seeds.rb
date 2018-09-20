65998f5f1820ebcf73afea61317f1adb6128b49c
ivonneandreasanchez <ivonneandreas@hotmail.com>
2018-09-20 14:45:18 -0500

    Modelo ER y Autenticación

--- JacDigital/db/seeds.rb
+++ JacDigital/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create (name: ‘María’, password:'123456', role_id: 1, email: 'ivonneandreas@hotmail.com', user: 'ivonneandreas', state_id: 1, birth: '')
+Role.create (name: 'Administrador', description: 'Administrador del sistema')
+State.create (name: 'Activo')
