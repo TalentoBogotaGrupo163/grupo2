fdee0f60e73be8e4bb4c6718944140276c80dc07
Viviana Londono <vivianalondono5@gmail.com>
2018-09-20 20:25:38 -0500

    cambio vivi 20092018

--- JacDigital/app/models/document_type.rb
+++ JacDigital/app/models/document_type.rb
@@ -1,3 +1,6 @@
 class DocumentType < ApplicationRecord
+    #relaciones
     has_many :documents
+    #Validaciones
+    validates :name, presence:{message:"Por favor ingresar Nombre del Tipo de documento."}
 end
