124809b7f2b0a3ac469f9965090b51a2f0190962
Viviana Londono <vivianalondono5@gmail.com>
2018-09-26 20:27:48 -0500

    actualiza validaciones

--- JacDigital/app/models/company.rb
+++ JacDigital/app/models/company.rb
@@ -1,8 +1,14 @@
 class Company < ApplicationRecord
+  #relaciones
   belongs_to :location
   has_and_belongs_to_many :phones
   has_many :menbers
   has_many :documents
   has_many :networks
   has_one :police_areas
+
+  #validaciones
+  validates :name, presence:{message:"Campo Obligatorio"}
+  validates :address, presence:{message:"Campo Obligatorio"}
+  
 end
--- JacDigital/db/seeds.rb
+++ JacDigital/db/seeds.rb
@@ -6,16 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> fdee0f60e73be8e4bb4c6718944140276c80dc07
+# Crea usuario de ejemplo por el problema de la autenticación
 Role.create(name: 'Administrador', description: 'Administrador del sistema')
 State.create(name: 'Activo')
 User.create(name: 'María', password:'123456', role_id: 1, email: 'ivonneandreas@hotmail.com', user: 'ivonneandreas', state_id: 1, birth: '')
 
-<<<<<<< HEAD
-=======
+# Crea tipos de documentos por defecto
 DocumentType.create(name: 'Constitución de junta de acción comunal de barrio, sector  y vereda')
 DocumentType.create(name: 'Constitución cuando existe segregación territorial')
 DocumentType.create(name: 'Modificación territorial del radio de acción ')
@@ -41,4 +37,24 @@ DocumentType.create(name: 'Procesos disciplinarios ')
 DocumentType.create(name: 'Recusación en el proceso disciplinario ')
 DocumentType.create(name: 'Procedimiento de atención para contratos de comodato lote y sede propiedad del municipio')
 
->>>>>>> fdee0f60e73be8e4bb4c6718944140276c80dc07
+# Crea localidades por defecto
+Location.create(name: 'Usaquén', mayor_name: 'alcalde1',id_number: '1',city_id: '1')
+Location.create(name: 'Chapinero', mayor_name: 'alcalde1',id_number: '2',city_id: '1')
+Location.create(name: 'Santa Fe', mayor_name: 'alcalde1',id_number: '3',city_id: '1')
+Location.create(name: 'San Cristóbal', mayor_name: 'alcalde1',id_number: '4',city_id: '1')
+Location.create(name: 'Usme', mayor_name: 'alcalde1',id_number: '5',city_id: '1')
+Location.create(name: 'Tunjuelito', mayor_name: 'alcalde1',id_number: '6',city_id: '1')
+Location.create(name: 'Bosa', mayor_name: 'alcalde1',id_number: '7',city_id: '1')
+Location.create(name: 'Kennedy', mayor_name: 'alcalde1',id_number: '8',city_id: '1')
+Location.create(name: 'Fontibón', mayor_name: 'alcalde1',id_number: '9',city_id: '1')
+Location.create(name: 'Engativá', mayor_name: 'alcalde1',id_number: '10',city_id: '1')
+Location.create(name: 'Suba', mayor_name: 'alcalde1',id_number: '11',city_id: '1')
+Location.create(name: 'Barrios Unidos', mayor_name: 'alcalde1',id_number: '12',city_id: '1')
+Location.create(name: 'Teusaquillo', mayor_name: 'alcalde1',id_number: '13',city_id: '1')
+Location.create(name: 'Los Mártires', mayor_name: 'alcalde1',id_number: '14',city_id: '1')
+Location.create(name: 'Antonio Nariño', mayor_name: 'alcalde1',id_number: '15',city_id: '1')
+Location.create(name: 'Puente Aranda', mayor_name: 'alcalde1',id_number: '16',city_id: '1')
+Location.create(name: 'La Candelaria', mayor_name: 'alcalde1',id_number: '17',city_id: '1')
+Location.create(name: 'Rafael Uribe Uribe', mayor_name: 'alcalde1',id_number: '18',city_id: '1')
+Location.create(name: 'Ciudad Bolívar', mayor_name: 'alcalde1',id_number: '19',city_id: '1')
+Location.create(name: 'Sumapaz', mayor_name: 'alcalde1',id_number: '20',city_id: '1')
